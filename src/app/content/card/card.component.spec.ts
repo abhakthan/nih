20772557ecc7d4677ec51fc2aa52271995293f4e
abhakthan <bhakthan@hotmail.com>
2022-12-16 00:26:49 -0500

    Add files via upload

---
+++ src/app/content/card/card.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardComponent } from './card.component';
+
+describe('CardComponent', () => {
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CardComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(CardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should trigger delete item', () => {
+    spyOn(component.remove, 'emit');
+    component.delete({});
+    expect(component.remove.emit).toHaveBeenCalled();
+  });
+});
--- src/app/content/card/card.component.ts
+++ src/app/content/card/card.component.ts
@@ -1,1 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+
+  @Input()
+  public user: any;
+
+  @Output()
+  public remove = new EventEmitter();
+
+  /**
+   * Delete item from the list
+   * @param user selected item
+   */
+  public delete(item: any) {
+    this.remove.emit(item);
+  }
+}
